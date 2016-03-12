297eb367958cb966da6fc490034bba59db7b72e8
Albert Julius Liu <ajul1987@gmail.com>
2016-03-12 18:30:22 -0500

    generate permutations

--- long_period/long_period.py
+++ long_period/long_period.py
@@ -18,10 +18,39 @@ by Ares Lagae and Philip Dutré.
 }
 """
 
-# List of primes. We don't need too many: we want to keep the permutation tables
-# small.
-primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+import numpy
+
+output_filename = 'hash.h'
+
+# List of primes. We don't need too many:
+# we want to keep the permutation tables small.
+# Also ignore small primes since they have less effect per operation.
+primes = [7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
           67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
           131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
           193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257]
 
+# Choose the least set of consecutive primes such that
+# the ratio between the least and greatest is less than a threshold.
+def selectPrimes(count, maximumRatio):
+    for primeIndex in range(len(primes) - count):
+        least = primes[primeIndex]
+        greatest = primes[primeIndex + count - 1]
+        if greatest / least < maximumRatio: 
+            return primes[primeIndex:(primeIndex+count)]
+    raise ValueError("No set of primes within range.")
+
+def generateHeader(count, maximumRatio):
+    permutations = [numpy.random.permutation(prime) for prime in
+                    selectPrimes(count, maximumRatio)]
+    result = ''
+    for permutation in permutations:
+        result += '__constant__ unsigned int permutation%d[%d] = {' % (
+            len(permutation),
+            len(permutation))
+        result += ', '.join(str(x) for x in permutation)
+        result += '};\n'
+
+    return result
+
+print(generateHeader(5, 2.2))
